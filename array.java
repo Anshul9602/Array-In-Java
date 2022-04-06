ecd4bfeeeaf46a25dd30d4c387e198d7e232baa5
Anshul <81064196+Anshul9602@users.noreply.github.com>
2022-04-06 11:12:55 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,18 @@
+public class array{
+    public static void main(String[] args) {
+        int []num= new int[7];
+        num [01] =10;
+        num [02] =20;
+        num [03] =30;
+        num [04] =40;
+        num [05] =50;
+        num [06] =60;
+        for(int i=0; i<num.length;i++){
+            System.out.print(num[i]);
+            System.out.print(" ");
+
+
+        }
+        
+    }
+}
